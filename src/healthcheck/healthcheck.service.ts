147f135d0525a77d16a8a2786d34e2d0fc966d5a
chebelok <lotokhinivan@gmail.com>
2024-01-25 17:29:17 +0200

    added Dockerfile & docker-compose

--- src/healthcheck/healthcheck.service.ts
+++ src/healthcheck/healthcheck.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { HealthcheckDto } from './dto/healthcheck.dto';
-import { Statuses } from 'src/enums/statuses.enum';
+import { Statuses } from '../enums/statuses.enum';
 
 @Injectable()
 export class HealthcheckService {
   async getStatus(): Promise<HealthcheckDto> {
     const randomIndex = Math.floor((Math.random() * 3));
-    
+
     return {
       status: Object.values(Statuses)[randomIndex],
       date: new Date()
